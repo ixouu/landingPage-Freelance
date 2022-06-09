7413b2759035b2f92accd3373409ec6f28ca4b7c
ixouu <julienchanon@gmail.com>
2022-06-10 00:01:05 +0200

    mobile responsive has been nearly finished

---
+++ assets/scripts/form.js
@@ -0,0 +1,40 @@
+const nameInput = document.querySelector('#nameInput');
+const emailInput = document.querySelector('#emailInput');
+const messageInput = document.querySelector('#messageInput');
+const inpSpan = document.querySelectorAll('.required');
+const inpIcons = document.querySelectorAll('.checkIcon');
+
+nameInput.addEventListener('input', (e)=>{
+    if(e.target.value.length <= 3){
+        inpSpan[0].style.opacity = '1';
+        inpIcons[0].src = "./assets/images/svg/error.svg";
+        inpIcons[0].style.display = 'inline';
+    } else {
+        inpSpan[0].style.opacity= '0';
+        inpIcons[0].src = "./assets/images/svg/check.svg";
+        inpIcons[0].style.display = 'inline';
+    }
+});
+
+
+emailInput.addEventListener('input', (e)=>{
+    const regexEmail = /\S+@\S+\.\S+/;
+    if (e.target.value.search(regexEmail) === 0){
+        inpSpan[1].style.opacity = '0';
+        inpIcons[1].src = "./assets/images/svg/check.svg";
+        inpIcons[1].style.display = 'block';
+    }else if (e.target.value.search(regexEmail) === -1){
+        inpSpan[1].style.opacity= '1';
+        inpIcons[1].src = "./assets/images/svg/error.svg";
+        inpIcons[1].style.display = 'block';
+    }
+});
+
+messageInput.addEventListener('input', (e) =>{
+    if(e.target.value.length <= 10){
+        inpSpan[2].style.opacity = "1";
+    }else{
+        inpSpan[2].style.opacity = "0";
+    }
+});
+console.log(inpSpan[2]);
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,18 +10,16 @@ const services = document.querySelector('#services');
 const portfolio = document.querySelector('#portfolio');
 const competences = document.querySelector('#competences');
 const testimonial = document.querySelector('#testimonial');
-const form = document.querySelector('form');
 const navMenuAcceuil = document.querySelector('.nav--menu__Acceuil');
 const navMenuServices = document.querySelector('.nav--menu__Services');
 const navMenuPortfolio = document.querySelector('.nav--menu__Portfolio');
 const navMenuCompetences = document.querySelector('.nav--menu__Competences');
 const navMenuTestimonial = document.querySelector('.nav--menu__Testimonial');
 const navMenuContact = document.querySelector('.nav--menu__Contact');
-const nameInput = document.querySelector('#nameInput');
-const emailInput = document.querySelector('#emailInput');
-const messageInput = document.querySelector('#messageInput');
-const NameRequiredSpan = document.querySelector('.nameRequired');
-const emailRequiredSpan = document.querySelector('.emailRequired');
+const form = document.querySelector('form');
+const iconMobile = document.querySelector('.mobile--menu__icon');
+const mobileMenu = document.querySelector('.mobile--nav');
+
 
 const options = {
     root : null,
@@ -156,37 +154,12 @@ function fixedNav() {
   }
 }
 
-nameInput.addEventListener('input', ()=>{
-    nameInput.setCustomValidity('');
-    nameInput.checkValidity();
-    nameInput.reportValidity();
-    if (nameInput.reportValidity() == true){
-        NameRequiredSpan.textContent= "";
-    }
+//iconMobileIcontoggle
+// function toggleMobileMenu(menu){
+//     menu.classList.toggle('open');
+// }
+iconMobile.addEventListener('click', () =>{
+    iconMobile.classList.toggle('open');
+    mobileMenu.classList.toggle('open')
 });
 
-nameInput.addEventListener('invalid', ()=>{
-    if(nameInput.value === ""){
-        nameInput.setCustomValidity('Veuillez saisir votre nom.');
-        NameRequiredSpan.textContent= "Veuillez saisir votre nom.";
-    } else{
-        nameInput.setCustomValidity('Vous n\' avez pas saisi correctement votre nom.');
-    }
-});
-emailInput.addEventListener('input', ()=>{
-    emailInput.setCustomValidity('');
-    emailInput.checkValidity();
-    emailInput.reportValidity();
-    if (emailInput.reportValidity() == true){
-        emailRequiredSpan.textContent= "";
-    }
-});
-
-emailInput.addEventListener('invalid', ()=>{
-    if(emailInput.value === ""){
-        emailInput.setCustomValidity('Veuillez écrire votre adresse mail');
-        emailRequiredSpan.textContent= "Veuillez rentrez un e-mail valide.";
-    }else{
-        emailInput.setCustomValidity('L\'adresse mail que vous avez écrite n\'est pas valide.')
-    }
-});
