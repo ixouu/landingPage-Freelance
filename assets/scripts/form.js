a6c9e70e58cbc24d97a8a3fb3e09586d23f40734
ixouu <julienchanon@gmail.com>
2022-06-24 00:09:49 +0200

    v1 finished

--- assets/scripts/form.js
+++ assets/scripts/form.js
@@ -4,31 +4,41 @@ const messageInput = document.querySelector('#messageInput');
 const inpSpan = document.querySelectorAll('.required');
 const inpIcons = document.querySelectorAll('.checkIcon');
 
-nameInput.addEventListener('input', (e)=>{
-    if(e.target.value.length <= 3){
-        inpSpan[0].style.opacity = '1';
-        inpIcons[0].src = "./assets/images/svg/error.svg";
-        inpIcons[0].style.display = 'inline';
-    } else {
-        inpSpan[0].style.opacity= '0';
-        inpIcons[0].src = "./assets/images/svg/check.svg";
-        inpIcons[0].style.display = 'inline';
-    }
-});
+nameInput.addEventListener('click', formNameCheck);
 
 
-emailInput.addEventListener('input', (e)=>{
-    const regexEmail = /\S+@\S+\.\S+/;
-    if (e.target.value.search(regexEmail) === 0){
-        inpSpan[1].style.opacity = '0';
-        inpIcons[1].src = "./assets/images/svg/check.svg";
-        inpIcons[1].style.display = 'block';
-    }else if (e.target.value.search(regexEmail) === -1){
-        inpSpan[1].style.opacity= '1';
-        inpIcons[1].src = "./assets/images/svg/error.svg";
-        inpIcons[1].style.display = 'block';
-    }
-});
+function formNameCheck (){
+    nameInput.addEventListener('input', (e)=>{
+        if(e.target.value.length <= 3){
+            inpSpan[0].style.opacity = '1';
+            inpIcons[0].src = "./assets/images/svg/error.svg";
+            inpIcons[0].style.display = 'inline';
+        } else {
+            inpSpan[0].style.opacity= '0';
+            inpIcons[0].src = "./assets/images/svg/check.svg";
+            inpIcons[0].style.display = 'inline';
+        }
+    });
+}
+
+emailInput.addEventListener('click', formEmailCheck);
+
+function formEmailCheck(){
+    emailInput.addEventListener('input', (e)=>{
+        const regexEmail = /\S+@\S+\.\S+/;
+        if (e.target.value.search(regexEmail) === 0){
+            inpSpan[1].style.opacity = '0';
+            inpIcons[1].src = "./assets/images/svg/check.svg";
+            inpIcons[1].style.display = 'block';
+        }else if (e.target.value.search(regexEmail) === -1){
+            inpSpan[1].style.opacity= '1';
+            inpIcons[1].src = "./assets/images/svg/error.svg";
+            inpIcons[1].style.display = 'block';
+        }
+    })
+}
+
+
 
 messageInput.addEventListener('input', (e) =>{
     if(e.target.value.length <= 10){
@@ -37,4 +47,3 @@ messageInput.addEventListener('input', (e) =>{
         inpSpan[2].style.opacity = "0";
     }
 });
-console.log(inpSpan[2]);
